b30b4433b5b36aefa16c9d902de107a39588cbbd
Herschel Pawar <pawarherschel@gmail.com>
2025-05-29 15:50:47 +0530

    1-intro/systems

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,37 @@ fn main() -> AppExit {
             }),
             ..default()
         }))
+        .add_systems(Startup, display_title)
         .run()
+}
+
+fn display_title(mut commands: Commands) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            flex_direction: FlexDirection::Column,
+            ..default()
+        },
+        children![
+            (
+                Text::new("Bevy Quickstart"),
+                TextFont {
+                    font_size: 130.0,
+                    ..default()
+                }
+            ),
+            (
+                Text::new("Rust Week 2025"),
+                TextFont {
+                    font_size: 100.0,
+                    ..default()
+                }
+            )
+        ],
+    ));
 }
