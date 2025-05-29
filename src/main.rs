f31f0b1c6a768a9daca198e3320a12a4d52b304d
Herschel Pawar <pawarherschel@gmail.com>
2025-05-29 16:29:49 +0530

    1-intro/plugins

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,7 @@ fn main() -> AppExit {
         }))
         .init_state::<GameState>()
         .enable_state_scoped_entities::<GameState>()
-        .add_systems(OnEnter(GameState::Splash), display_title)
-        .add_systems(Update, switch_to_menu.run_if(in_state(GameState::Splash)))
+        .add_plugins(splash::SplashPlugin)
         .run()
 }
 
@@ -23,49 +22,64 @@ enum GameState {
     StartMenu,
 }
 
-#[derive(Resource)]
-struct SplashScreenTimer(Timer);
+mod splash {
+    use bevy::prelude::*;
 
-fn display_title(mut commands: Commands) {
-    commands.spawn(Camera2d);
+    use crate::GameState;
 
-    commands.spawn((
-        Node {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            align_items: AlignItems::Center,
-            justify_content: JustifyContent::Center,
-            flex_direction: FlexDirection::Column,
-            ..default()
-        },
-        children![
-            (
-                Text::new("Bevy Quickstart"),
-                TextFont {
-                    font_size: 130.0,
-                    ..default()
-                }
-            ),
-            (
-                Text::new("Rust Week 2025"),
-                TextFont {
-                    font_size: 100.0,
-                    ..default()
-                }
-            )
-        ],
-        StateScoped(GameState::Splash),
-    ));
+    pub struct SplashPlugin;
 
-    commands.insert_resource(SplashScreenTimer(Timer::from_seconds(2.0, TimerMode::Once)));
-}
+    impl Plugin for SplashPlugin {
+        fn build(&self, app: &mut App) {
+            app.add_systems(OnEnter(GameState::Splash), display_title)
+                .add_systems(Update, switch_to_menu.run_if(in_state(GameState::Splash)));
+        }
+    }
 
-fn switch_to_menu(
-    mut next: ResMut<NextState<GameState>>,
-    mut timer: ResMut<SplashScreenTimer>,
-    time: Res<Time>,
-) {
-    if timer.0.tick(time.delta()).just_finished() {
-        next.set(GameState::StartMenu);
+    #[derive(Resource)]
+    struct SplashScreenTimer(Timer);
+
+    fn display_title(mut commands: Commands) {
+        commands.spawn(Camera2d);
+
+        commands.spawn((
+            Node {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                flex_direction: FlexDirection::Column,
+                ..default()
+            },
+            children![
+                (
+                    Text::new("Bevy Quickstart"),
+                    TextFont {
+                        font_size: 130.0,
+                        ..default()
+                    }
+                ),
+                (
+                    Text::new("Rust Week 2025"),
+                    TextFont {
+                        font_size: 100.0,
+                        ..default()
+                    }
+                )
+            ],
+            StateScoped(GameState::Splash),
+        ));
+
+        commands.insert_resource(SplashScreenTimer(Timer::from_seconds(2.0, TimerMode::Once)));
+    }
+
+    fn switch_to_menu(
+        mut next: ResMut<NextState<GameState>>,
+        mut timer: ResMut<SplashScreenTimer>,
+        time: Res<Time>,
+    ) {
+        if timer.0.tick(time.delta()).just_finished() {
+            next.set(GameState::StartMenu);
+        }
     }
 }
